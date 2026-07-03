47975b053b8dbbabfdaeb7b0f2a3bbf8681719e3
JOJO <39382270+GCJOJO@users.noreply.github.com>
2026-07-03 23:03:52 +0200

    Block stat and Killed Mob stat Tasks

--- common/src/main/java/io/github/gcjojo/questslib/quests/QuestManager.java
+++ common/src/main/java/io/github/gcjojo/questslib/quests/QuestManager.java
@@ -2,27 +2,28 @@ package io.github.gcjojo.questslib.quests;
 
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.BlockEvent;
+import dev.architectury.event.events.common.EntityEvent;
 import dev.architectury.event.events.common.PlayerEvent;
 import dev.architectury.utils.value.IntValue;
-import io.github.gcjojo.questslib.Questslib;
 import io.github.gcjojo.questslib.events.QuestsEvents;
 import io.github.gcjojo.questslib.quests.tasks.StatTask;
 import io.github.gcjojo.questslib.quests.tasks.StatTaskType;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class QuestManager {
 
@@ -42,21 +43,15 @@ public class QuestManager {
     }
 
     public static void loadQuests() {
-        ArrayList quest1Tasks = new ArrayList<QuestTask>();
-        quest1Tasks.add(new StatTask(ResourceLocation.tryBuild(Questslib.MOD_ID, "test_item_task"), Component.literal("Test Item Task"), Component.literal("Test Item Task"),
-                ResourceLocation.tryBuild(ResourceLocation.DEFAULT_NAMESPACE, "dirt"), 10, StatTaskType.Item));
 
-        quests.put(ResourceLocation.tryBuild(Questslib.MOD_ID, "test_item_quest"), new Quest(ResourceLocation.tryBuild(Questslib.MOD_ID, "test_item_quest"), Component.literal("Test Item Quest"), Component.literal("This quest test obtain items tasks"), quest1Tasks));
     }
 
-    @Nullable
-    public static Quest getQuest(ResourceLocation questId) {
-        return quests.getOrDefault(questId, null);
+    public static Optional<Quest> getQuest(ResourceLocation questId) {
+        return Optional.ofNullable(quests.getOrDefault(questId, null));
     }
 
     public static void onPlayerJoin(ServerPlayer player) {
         playerData.put(player, loadPlayerData(player));
-        playerData.get(player).put(ResourceLocation.tryBuild(Questslib.MOD_ID, "test_item_quest"), new PlayerQuestData(ResourceLocation.tryBuild(Questslib.MOD_ID, "test_item_quest")));
     }
 
     public static void onPlayerLeave(ServerPlayer player) {
@@ -68,54 +63,86 @@ public class QuestManager {
         PlayerEvent.PLAYER_JOIN.register(QuestManager::onPlayerJoin);
         PlayerEvent.PLAYER_QUIT.register(QuestManager::onPlayerLeave);
 
-        PlayerEvent.PICKUP_ITEM_POST.register(QuestManager::PlayerPickupItem);
-        BlockEvent.BREAK.register(QuestManager::PlayerBreakBlock);
-        BlockEvent.PLACE.register(QuestManager::EntityPlaceBlock);
+        PlayerEvent.PICKUP_ITEM_POST.register(QuestManager::onPlayerPickupItem);
+        BlockEvent.BREAK.register(QuestManager::onPlayerBreakBlock);
+        BlockEvent.PLACE.register(QuestManager::onEntityPlaceBlock);
+
+        EntityEvent.LIVING_DEATH.register(QuestManager::onEntityDie);
     }
 
-    public static void PlayerPickupItem(Player player, ItemEntity itemEntity, ItemStack stack) {
+    public static void onStatTaskUpdate(Player player, PlayerQuestData questData, ResourceLocation targetId, int amount, StatTaskType type) {
+        if (questData.getCompletionState() == QuestCompletionState.None || questData.getCompletionState() == QuestCompletionState.Completed)
+            return;
+        ResourceLocation questId = questData.getQuestId();
+        Quest quest = getQuest(questId).orElse(null);
+        if (quest == null) return;
+
+        QuestTask task = questData.getCurrentTask().orElse(null);
+        if (task == null) return;
+
+        if (task.getTaskType() != TaskType.Stat) return;
+
+        StatTask statTask = (StatTask) task;
+        if (statTask.getStatType() != type || statTask.getTargetId() != targetId) return;
+
+        if (!(questData.getCurrentTaskData() instanceof StatTask.StatTaskData statData)) return;
+
+        statData.addAmount(amount);
+
+        QuestsEvents.TASK_PROGRESSION.invoker().taskProgression(player, questId, task.getTaskId());
+
+        if (questData.checkTaskProgression()) {
+            QuestsEvents.TASK_COMPLETED.invoker().taskCompleted(player, task.getTaskId());
+
+            questData.nextTask();
+            if (questData.getCompletionState() == QuestCompletionState.Completed)
+                QuestsEvents.QUEST_COMPLETED.invoker().questCompleted(player, questId);
+        }
+    }
+
+    public static void onPlayerPickupItem(Player player, ItemEntity itemEntity, ItemStack stack) {
         if (!playerData.containsKey(player)) return;
 
         PlayerQuestDataMap dataMap = playerData.get(player);
         dataMap.forEach((questId, questData) -> {
-            if (questData.getCompletionState() == QuestCompletionState.None) return;
-
-            Quest quest = getQuest(questId);
-            if (quest == null) return;
-
-            QuestTask task = questData.getCurrentTask().orElse(null);
-            if (task == null) return;
-
-            if (task.getTaskType() != TaskType.Stat) return;
-
-            StatTask statTask = (StatTask) task;
-            if (statTask.getStatType() != StatTaskType.Item || statTask.getItem() != stack.getItem()) return;
-
-            if (!(questData.getCurrentTaskData() instanceof StatTask.StatTaskData statData)) return;
-
-            statData.addAmount(stack.getCount());
-
-            QuestsEvents.TASK_PROGRESSION.invoker().taskProgression(player, questId, task.getTaskId());
-
-            if (questData.checkTaskProgression()) {
-                QuestsEvents.TASK_COMPLETED.invoker().taskCompleted(player, task.getTaskId());
-
-                questData.nextTask();
-                if (questData.getCompletionState() == QuestCompletionState.Completed)
-                    QuestsEvents.QUEST_COMPLETED.invoker().questCompleted(player, questId);
-            }
+            onStatTaskUpdate(player, questData, BuiltInRegistries.ITEM.getKey(stack.getItem()), stack.getCount(), StatTaskType.Item);
         });
     }
 
-    public static EventResult PlayerBreakBlock(Level level, BlockPos pos, BlockState state, ServerPlayer player, IntValue xp) {
+    public static EventResult onPlayerBreakBlock(Level level, BlockPos pos, BlockState state, ServerPlayer player, IntValue xp) {
+        if (!playerData.containsKey(player)) return EventResult.pass();
+
+        PlayerQuestDataMap dataMap = playerData.get(player);
+        dataMap.forEach((questId, questData) -> {
+            onStatTaskUpdate(player, questData, BuiltInRegistries.BLOCK.getKey(state.getBlock()), 1, StatTaskType.BrokenBlocks);
+        });
 
         return EventResult.pass();
     }
 
-    public static EventResult EntityPlaceBlock(Level level, BlockPos pos, BlockState state, Entity placer) {
-        if (!(placer instanceof Player)) return EventResult.pass();
+    public static EventResult onEntityPlaceBlock(Level level, BlockPos pos, BlockState state, Entity placer) {
+        if (!(placer instanceof Player player)) return EventResult.pass();
 
-        Player player = (Player) placer;
+        if (!playerData.containsKey(player)) return EventResult.pass();
+
+        PlayerQuestDataMap dataMap = playerData.get(player);
+        dataMap.forEach((questId, questData) -> {
+            onStatTaskUpdate(player, questData, BuiltInRegistries.BLOCK.getKey(state.getBlock()), 1, StatTaskType.PlacedBlocks);
+        });
+
+        return EventResult.pass();
+    }
+
+    public static EventResult onEntityDie(LivingEntity entity, DamageSource source) {
+        Entity sourceEntity = source.getEntity();
+        if (!(sourceEntity instanceof Player player)) return EventResult.pass();
+
+        if (!playerData.containsKey(player)) return EventResult.pass();
+
+        PlayerQuestDataMap dataMap = playerData.get(player);
+        dataMap.forEach((questId, questData) -> {
+            onStatTaskUpdate(player, questData, BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()), 1, StatTaskType.KilledMobs);
+        });
 
         return EventResult.pass();
     }
